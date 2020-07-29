1f1559e5b7a03689f4ff3278c3f4a3c9440ef8f6
Yani Asari <yani.iman1@gmail.com>
2020-07-29 17:29:30 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(phrase = "Squawk!")
-  puts phrase = "Squawk!"
+def parrot
+  (phrase = "Squawk!") 
+  puts "Squawk!"
   
 end
